287159316ced268bf5c07a0a9151abddf3e214e0
Sahil Verma <domainsahil007@gmail.com>
2024-12-20 01:14:36 +0530

    feat: about me added with dummy data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import AboutMe from "@/components/sections/about-me";
 import Hero from "@/components/sections/hero-section";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <AboutMe />
     </div>
   );
 };
---
+++ src/components/sections/about-me.tsx
@@ -0,0 +1,19 @@
+import AnimationContainer from "../animated/animated-container";
+
+const AboutMe = () => {
+  return (
+    <AnimationContainer customClassName="w-full py-12 lg:py-16">
+      <h2 className="mb-8 text-2xl font-bold tracking-tight text-center text-white lg:text-start">
+        About me
+      </h2>
+
+      <p className="w-full text-base font-normal leading-7 text-justify text-neutral-200">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic deleniti
+        quam eaque! Repudiandae quas accusantium officia hic provident animi
+        exercitationem delectus error nesciunt voluptas, tempore sed ab
+        consequatur eos sunt.
+      </p>
+    </AnimationContainer>
+  );
+};
+export default AboutMe;
