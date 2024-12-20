30a02c3117d08e24826a7c49a05b1b2e09b17e12
Sahil Verma <domainsahil007@gmail.com>
2024-12-20 09:51:13 +0530

    feat: projects card and page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,36 @@
+import AnimationContainer from "@/components/animated/animated-container";
 import AnimatedTitle from "@/components/animated/animated-title";
+import ProjectCard from "@/components/project-card";
 
 const Projects = () => {
   return (
     <div>
       <AnimatedTitle title="Projects" />
+      <AnimationContainer>
+        <p className="w-full text-base font-normal leading-7 text-justify text-neutral-200">
+          I&apos;ve built a variety of projects ranging from websites, apps,
+          extensions, open source projects and more.
+        </p>
+        <div className="grid grid-cols-1 gap-3 py-6 lg:py-10 sm:grid-cols-2">
+          {Array.from({ length: 6 }).map((_, index) => (
+            <ProjectCard
+              key={index}
+              project={{
+                dates: "2023",
+                description: "This is a sample project",
+                github: "https://github.com",
+                href: "https://github.com",
+                image:
+                  "https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+                links: "https://github.com",
+                stack: ["React", "Next.js", "TailwindCSS"],
+                title: "Sample Project",
+                type: "extension",
+              }}
+            />
+          ))}
+        </div>
+      </AnimationContainer>
     </div>
   );
 };
--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -1,10 +1,98 @@
 import { Project } from "@/interfaces";
 
+// icons
+// import { RiNpmjsLine as NpmIcon } from "react-icons/ri";
+import {
+  // FaMobileScreenButton as MobileIcon,
+  // FaChrome as ChromeIcon,
+  // FaGlobe as WebIcon,
+  FaGithub as GithubIcon,
+} from "react-icons/fa6";
+import { MdOpenInNew as OpenIcon } from "react-icons/md";
+
+import { Badge } from "./ui/badge";
+import AnimationContainer from "./animated/animated-container";
+import Link from "next/link";
+
 interface Props {
   project: Project;
 }
 
+// const ProjectIcon: React.FC<{ type: Project["type"] }> = ({ type }) => {
+//   switch (type) {
+//     case "extension":
+//       return <ChromeIcon />;
+//     case "mobile":
+//       return <MobileIcon />;
+//     case "package":
+//       return <NpmIcon />;
+//     case "web":
+//       return <WebIcon />;
+//     default:
+//       return <WebIcon />;
+//   }
+// };
+
 const ProjectCard: React.FC<Props> = ({ project }) => {
-  return <div className="w-full border">{project.title}</div>;
+  return (
+    <AnimationContainer>
+      <div className="w-full h-auto group border backdrop-blur border-white/10 space-y-2 md:space-y-4 rounded-2xl p-4">
+        <div className="relative rounded-2xl overflow-hidden">
+          <Badge className="absolute bottom-2 left-2 uppercase">
+            {project.type}
+          </Badge>
+          <div className="absolute flex items-center gap-2 top-2 right-2 ">
+            {/* <ProjectIcon type={project.type} /> */}
+            <a
+              href={project.links}
+              target="_blank"
+              rel="noreferrer"
+              className="p-4 bg-black/10 rounded-full backdrop-blur hover:bg-black/30 transition-all"
+            >
+              <OpenIcon />
+            </a>
+            <a
+              href={project.links}
+              target="_blank"
+              rel="noreferrer"
+              className="p-4 bg-black/10 rounded-full backdrop-blur hover:bg-black/30 transition-all"
+            >
+              <GithubIcon />
+            </a>
+          </div>
+          <Link href={project.href}>
+            <img
+              src={project.image}
+              alt={project.title}
+              className=" w-full aspect-video h-full object-cover object-top "
+            />
+          </Link>
+          {/* <div className="overlay" /> */}
+        </div>
+        <div className="space-y-2">
+          <Link href={project.href}>
+            <h3 className="font-semibold group-hover:underline group-hover:text-primary transition-all tracking-tight mt-1 text-base md:text-xl">
+              {project.title}
+            </h3>
+          </Link>
+          <time className="font-sans text-xs">{project.dates}</time>
+          <div className="hidden font-sans text-xs underline print:visible" />
+          <div className="prose max-w-full text-pretty font-sans text-xs text-muted-foreground dark:prose-invert">
+            <p>{project.description}</p>
+          </div>
+        </div>
+        <div className=" flex flex-wrap gap-2">
+          {project.stack.map((stack) => (
+            <Badge
+              key={stack}
+              className="bg-white/10 hover:bg-white/20 backdrop-blur"
+            >
+              {stack}
+            </Badge>
+          ))}
+        </div>
+      </div>
+    </AnimationContainer>
+  );
 };
 export default ProjectCard;
