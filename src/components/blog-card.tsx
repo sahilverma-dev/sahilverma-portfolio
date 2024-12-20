0d8c2c810c912f825859556acde370853a61b2f2
Sahil Verma <domainsahil007@gmail.com>
2024-12-20 14:23:23 +0530

    feat: single blog page

--- src/components/blog-card.tsx
+++ src/components/blog-card.tsx
@@ -23,16 +23,16 @@ const BlogCard = () => {
             <time dateTime="2020-03-16" className="text-base-500">
               <time dateTime="2024-12-02T00:00:00.000Z"> Dec 2, 2024 </time>
             </time>
-            <Link href="/blog-template/posts/comments-via-bluesky/">
+            <Link href="/blog/blog-template">
               <Badge className="bg-white/10 backdrop-blur">lorem</Badge>
             </Link>
           </div>
           <div className="max-w-xl">
             <div className="mt-3 text-lg font-semibold leading-6 text-base-900 dark:text-base-50">
-              <a href="/blog-template/posts/comments-via-bluesky/">
+              <Link href="/blog/blog-template">
                 <span className="absolute inset-0" /> Lorem ipsum dolor sit
                 amet.
-              </a>
+              </Link>
               <div className="absolute -inset-2 md:-inset-4 rounded-2xl opacity-0 group-hover:opacity-100 scale-95 group-hover:scale-100 bg-white/10 backdrop-blur -z-10 transition-all duration-150" />
             </div>
             <p className="mt-5 text-sm leading-6 text-base-600 dark:text-base-400">
