784ff8ba7afc75df1fd394b4854b289013c494f4
Sahil Verma <domainsahil007@gmail.com>
2024-12-23 20:45:14 +0530

    fix: FIXED MY NAME TYPO DAMN

--- src/components/sections/about-me.tsx
+++ src/components/sections/about-me.tsx
@@ -8,9 +8,9 @@ const AboutMe = () => {
       </h2>
 
       <p className="w-full text-base font-normal leading-7 text-justify text-neutral-200">
-        Hi, I&apos;m Sahil Verm! I&apos;m a passionate developer with a love for
-        turning ideas into digital experiences. With a focus on clean code and
-        creative solutions, I enjoy building projects that solve real-world
+        Hi, I&apos;m Sahil Verma! I&apos;m a passionate developer with a love
+        for turning ideas into digital experiences. With a focus on clean code
+        and creative solutions, I enjoy building projects that solve real-world
         problems. When I&apos;m not coding, I&apos;m exploring new technologies,
         learning, and sharing my journey through blog posts. Join me as I
         explore the intersection of tech and creativity!
