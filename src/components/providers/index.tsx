490906291a1f549b0b0272fca2856b6f3706cec6
Sahil Verma <domainsahil007@gmail.com>
2024-12-28 20:57:46 +0530

    feat: top loader added

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,6 +1,12 @@
 import { CSPostHogProvider } from "./posthog-provider";
+import NextTopLoader from "nextjs-toploader";
 
 const Providers = ({ children }: React.PropsWithChildren) => {
-  return <CSPostHogProvider>{children}</CSPostHogProvider>;
+  return (
+    <CSPostHogProvider>
+      <NextTopLoader color="#eb5200" showSpinner={false} />
+      {children}
+    </CSPostHogProvider>
+  );
 };
 export default Providers;
