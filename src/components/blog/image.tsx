1ef5a58b107e5d0d48762e9a96feadda8dcd2276
Sahil Verma <domainsahil007@gmail.com>
2025-01-11 18:36:05 +0530

    fix: blog image improved

--- src/components/blog/image.tsx
+++ src/components/blog/image.tsx
@@ -1,15 +1,18 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { useEffect, useState } from "react";
-import { buttonVariants } from "../ui/button";
-import { AnimatePresence, HTMLMotionProps, motion } from "motion/react";
-import { XIcon } from "lucide-react";
+import { useEffect, useMemo, useState } from "react";
+import { buttonVariants } from "@/components/ui/button";
+import { motion, AnimatePresence } from "motion/react";
+import { X as XIcon } from "lucide-react";
+
 import Image from "next/image";
 
-type BlogImageProps = React.ImgHTMLAttributes<
-  HTMLImageElement | Omit<HTMLMotionProps<"img">, "ref">
->;
+interface BlogImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+  src: string;
+  alt?: string;
+  className?: string;
+}
 
 const BlogImage: React.FC<BlogImageProps> = ({
   src,
@@ -17,6 +20,8 @@ const BlogImage: React.FC<BlogImageProps> = ({
   className = "",
 }) => {
   const [open, setOpen] = useState(false);
+
+  const layoutId = useMemo(() => crypto.randomUUID(), []);
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -30,31 +35,38 @@ const BlogImage: React.FC<BlogImageProps> = ({
     };
   }, []);
 
+  if (!src) return null;
+
   return (
     <>
-      {src && (
-        <motion.div
-          // TODO: add layout
-          // layout
-          // layoutId="blog-image"
-          className={`relative rounded-2xl my-4 overflow-hidden cursor-pointer`}
-          onClick={() => setOpen(true)}
-        >
-          <Image
-            src={src}
-            alt={alt}
-            className={cn(
-              "w-full h-full object-cover transition-opacity duration-300",
-              className
-            )}
-            height={960}
-            width={960}
-          />
-        </motion.div>
-      )}
+      <motion.div
+        layout
+        layoutId={layoutId}
+        className="relative max-h-[600px] rounded-2xl overflow-hidden cursor-pointer"
+        onClick={() => setOpen(true)}
+      >
+        <Image
+          src={src}
+          alt={alt}
+          className={cn(
+            "w-full h-auto object-contain transition-opacity duration-300 !m-0",
+            className
+          )}
+          height={960}
+          width={960}
+        />
+      </motion.div>
+      <motion.p
+        layout
+        layoutId={`${layoutId}-p`}
+        className="text-center text-xs text-gray-400 mt-2 mb-8"
+      >
+        {alt}
+      </motion.p>
+
       <AnimatePresence mode="wait">
         {open && (
-          <div className="fixed inset-0 z-[99999999]">
+          <div className="fixed inset-0 z-[999999]">
             <motion.button
               initial={{ opacity: 0, x: 30 }}
               animate={{ opacity: 1, x: 0 }}
@@ -67,6 +79,7 @@ const BlogImage: React.FC<BlogImageProps> = ({
             >
               <XIcon />
             </motion.button>
+
             <motion.div
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
@@ -74,17 +87,32 @@ const BlogImage: React.FC<BlogImageProps> = ({
               className="absolute inset-0 h-full bg-black/50 backdrop-blur cursor-pointer"
               onClick={() => setOpen(false)}
             />
-            <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-full max-w-5xl p-4">
-              <motion.img
-                // layout
-                // layoutId="blog-image"
-                src={src}
-                alt={alt}
-                className={cn(
-                  "w-full h-full object-cover transition-opacity duration-300 rounded-3xl",
-                  className
-                )}
-              />
+
+            <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-full max-w-5xl p-4 md:p-0">
+              <motion.div
+                layout
+                layoutId={layoutId}
+                className="w-full h-full flex items-center justify-center"
+              >
+                <Image
+                  src={src}
+                  alt={alt}
+                  className={cn(
+                    "w-auto max-w-full h-auto max-h-[80vh] object-contain transition-opacity duration-300 rounded-3xl !m-0",
+                    className
+                  )}
+                  width={1920}
+                  height={1080}
+                />
+              </motion.div>
+
+              <motion.p
+                layout
+                layoutId={`${layoutId}-p`}
+                className="text-center text-xs text-gray-400 mt-2 mb-8"
+              >
+                {alt}
+              </motion.p>
             </div>
           </div>
         )}
