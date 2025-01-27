1d92d4482243afe1f68f6a4551ef9abf61690fdd
Sahil Verma <domainsahil007@gmail.com>
2025-01-27 11:24:12 +0530

    chore: dynamic import mdx components

--- src/components/mdx-content.tsx
+++ src/components/mdx-content.tsx
@@ -1,43 +1,48 @@
+import dynamic from "next/dynamic";
 import { JSX } from "react";
-
 import { MDXRemote, MDXRemoteProps } from "next-mdx-remote/rsc";
-import BlogImage from "./blog/image";
-import CodeBlock from "./blog/code-block";
-import Youtube from "./blog/youtube";
-import CallOut from "./blog/call-out";
-import SimpleFadeInAnimation from "./blog/motion-crash-course/simple-fade-in-animation";
-import AnimationVariants from "./blog/motion-crash-course/animation-variants";
-import HoverAnimation from "./blog/motion-crash-course/hover-animation";
-import OverViewAnimation from "./blog/motion-crash-course/overview-animation";
-import DraggableBox from "./blog/motion-crash-course/draggable-box";
-import LayoutAnimation from "./blog/motion-crash-course/layout-animation";
-import StaggeredAnimation from "./blog/motion-crash-course/staggered-animation";
-import ExitAnimation from "./blog/motion-crash-course/exit-animation";
-import LayoutIdAnimation from "./blog/motion-crash-course/layoutid-animation";
-import MotionValueAnimation from "./blog/motion-crash-course/motion-value";
+
+// Dynamic imports using `next/dynamic`
+const components = {
+  img: dynamic(() => import("./blog/image")),
+  // code: dynamic(() => import("./blog/new-code-block")),
+  code: dynamic(() => import("./blog/code-block")),
+  Youtube: dynamic(() => import("./blog/youtube")),
+  CallOut: dynamic(() => import("./blog/call-out")),
+  SimpleFadeInAnimation: dynamic(
+    () => import("./blog/motion-crash-course/simple-fade-in-animation")
+  ),
+  AnimationVariants: dynamic(
+    () => import("./blog/motion-crash-course/animation-variants")
+  ),
+  HoverAnimation: dynamic(
+    () => import("./blog/motion-crash-course/hover-animation")
+  ),
+  OverViewAnimation: dynamic(
+    () => import("./blog/motion-crash-course/overview-animation")
+  ),
+  DraggableBox: dynamic(
+    () => import("./blog/motion-crash-course/draggable-box")
+  ),
+  LayoutAnimation: dynamic(
+    () => import("./blog/motion-crash-course/layout-animation")
+  ),
+  StaggeredAnimation: dynamic(
+    () => import("./blog/motion-crash-course/staggered-animation")
+  ),
+  ExitAnimation: dynamic(
+    () => import("./blog/motion-crash-course/exit-animation")
+  ),
+  LayoutIdAnimation: dynamic(
+    () => import("./blog/motion-crash-course/layoutid-animation")
+  ),
+  MotionValueAnimation: dynamic(
+    () => import("./blog/motion-crash-course/motion-value")
+  ),
+};
 
 export default function MDXContent(
   props: JSX.IntrinsicAttributes & MDXRemoteProps
 ) {
-  return (
-    <MDXRemote
-      {...props}
-      components={{
-        img: BlogImage,
-        code: CodeBlock,
-        Youtube,
-        CallOut,
-        SimpleFadeInAnimation,
-        AnimationVariants,
-        HoverAnimation: HoverAnimation,
-        OverViewAnimation: OverViewAnimation,
-        DraggableBox: DraggableBox,
-        LayoutAnimation: LayoutAnimation,
-        LayoutIdAnimation: LayoutIdAnimation,
-        StaggeredAnimation: StaggeredAnimation,
-        ExitAnimation: ExitAnimation,
-        MotionValueAnimation: MotionValueAnimation,
-      }}
-    />
-  );
+  return <MDXRemote {...props} components={components} />;
 }
